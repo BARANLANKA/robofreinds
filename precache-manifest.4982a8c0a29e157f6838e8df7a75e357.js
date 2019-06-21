865b49827b234d82d040e6f12cc76661c1191e3a
BARANLANKA <ivan@novikov.lv>
2019-06-21 13:09:43 +0530

    Updates

---
+++ precache-manifest.4982a8c0a29e157f6838e8df7a75e357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577b0d08645c2bf5248a4c9904675a7d",
+    "url": "/robofreinds/index.html"
+  },
+  {
+    "revision": "ab9085063eae4ed4687d",
+    "url": "/robofreinds/static/css/2.2ec98f22.chunk.css"
+  },
+  {
+    "revision": "fd090f8292c230148a93",
+    "url": "/robofreinds/static/css/main.b9087a98.chunk.css"
+  },
+  {
+    "revision": "ab9085063eae4ed4687d",
+    "url": "/robofreinds/static/js/2.a537082c.chunk.js"
+  },
+  {
+    "revision": "fd090f8292c230148a93",
+    "url": "/robofreinds/static/js/main.688cc16e.chunk.js"
+  },
+  {
+    "revision": "8a7423c08e95ca1d19f7",
+    "url": "/robofreinds/static/js/runtime~main.1a9ba4dd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofreinds/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/robofreinds/precache-manifest.4982a8c0a29e157f6838e8df7a75e357.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofreinds/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
